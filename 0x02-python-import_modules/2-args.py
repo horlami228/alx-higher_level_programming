e701feb21a3941bab0b7759f7f73f2b4445791d5
horlami228 <akintolaolamilekan51@gmail.com>
2022-12-24 13:29:36 +0100

    add

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-
+    import sys
     if len(sys.argv) == 1:
         print("{} arguments.".format(len(sys.argv)-1))
 
     else:
         print("{} arguments:".format(len(sys.argv)-1))
 
-    n = len(sys.argv)
+    n = len(sys.argv)-1
 
-    for string in range(1, n):
-        print("{}: {}".format(string, sys.argv[string]))
+    for string in range(n):
+        print("{}: {}".format(string, sys.argv[string + 1]))
