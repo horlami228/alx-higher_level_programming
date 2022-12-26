7433ebc85bd9efc48eab34f9ebfe6d1effe79432
horlami228 <akintolaolamilekan51@gmail.com>
2022-12-27 00:40:56 +0100

    add

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ adds two tuples
 """
+
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    """ this function adds 2 tuples"""
     if len(tuple_a) > 2:
         tuple_a = (tuple_a[0], tuple_a[1])
     if len(tuple_b) > 2:
@@ -17,4 +18,3 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_b = (0, 0)
     sum_tupple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
     return sum_tupple
-
