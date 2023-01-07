f3795020196d00f468febcb1ba2dfb15eca581a9
horlami228 <akintolaolamilekan51@gmail.com>
2023-01-07 23:44:47 +0100

    add

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,7 +4,6 @@
 
 class Square:
     def __init__(self, size=0):
-        self.__size = size
         """initialize with a private instance size
         and an optional argument"""
         if not isinstance(size, int):
@@ -13,3 +12,4 @@ class Square:
         if size < 0:
             """if size < 0"""
             raise ValueError("size must be >= 0")
+        self.__size = size
