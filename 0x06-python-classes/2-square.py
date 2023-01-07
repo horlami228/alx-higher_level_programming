3d4e6c6656a2dacaf3999d3f66d381a9416a83f9
horlami228 <akintolaolamilekan51@gmail.com>
2023-01-07 23:40:30 +0100

    add

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""define a square class"""
+
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+        """initialize with a private instance size
+        and an optional argument"""
+        if not isinstance(size, int):
+            """check if size is int"""
+            raise TypeError("size must be an integer")
+        if size < 0:
+            """if size < 0"""
+            raise ValueError("size must be >= 0")
