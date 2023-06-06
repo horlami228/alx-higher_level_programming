d17398275a2d10370874c667fc0241f3df06fff0
Olamilekan <akintolaolamilekan51@gmail.com>
2023-06-07 00:16:23 +0100

    updated

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,6 @@
 # This program prints 1 to 99 with each number separated by a comma and space
 for num in range(0, 100):
     if num == 99:
-        print("{}".format(num))
+        print("{:d}".format(num))
     else:
         print("{:02}".format(num), end=", ")
