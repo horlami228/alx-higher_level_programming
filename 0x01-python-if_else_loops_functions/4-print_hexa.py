71eb181e69d019e68bd1ca886412674f6f2cd623
Olamilekan <akintolaolamilekan51@gmail.com>
2023-06-07 00:18:42 +0100

    updated

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -2,4 +2,5 @@
 
 # This program prints all decimal and hexadecimal number from 0 to 96
 for num in range(0, 99):
-    print(f"{num:d} = {hex(num)}") # The hex function converts and integer to its correspond hex format
+    print("{} = {}".format(num, hex(num)))
+    # The hex function converts and integer to its correspond hex format
