c7e06b416dfad3bf2991dc9fd1d797b4cbdf24bd
Olamilekan <akintolaolamilekan51@gmail.com>
2023-08-16 12:11:15 +0100

    mysql

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- update a record in the table
+
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- delete from the records that the score was <= 10
+
+DELETE FROM second_table WHERE score <= 10;
