bf8b3f8079c3e396544b367a0c3417670b392c1a
Olamilekan <akintolaolamilekan51@gmail.com>
2023-08-16 13:02:14 +0100

    mysql

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,4 +1,4 @@
 -- compute the average of the scores in the table
 -- print result column as average
 
-SELECT SUM(score) / 2 AS average FROM second_table;
+SELECT AVG(score) AS average FROM second_table;
