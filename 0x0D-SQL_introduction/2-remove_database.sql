e438c25f755673094085348cd6cb841c7c3ec223
Olamilekan <akintolaolamilekan51@gmail.com>
2023-08-16 06:40:51 +0100

    MYSQL

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- This command to delete a database
+-- If not exists do nothing
+
+DROP DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- select the database
+-- list all tables in the database
+
+USE mysql;
+SHOW TABLES;
