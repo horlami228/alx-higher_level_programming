83756337d4381d1190da0f94b28191c1c337cead
Olamilekan <akintolaolamilekan51@gmail.com>
2023-08-17 20:58:12 +0100

    moremysql

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- list all privileges of user_0d_1 and user_0d_2
+
+SHOW GRANT FOR 'user_0d_1'@'localhost';
+
+SHOW GRANT FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/README.md
@@ -0,0 +1,6 @@
+# MYSQL More Queries
+
+## Project Folders
+
+[0-privileges.sql](0-privileges.sql)
+* > * list all privileges granted to a user
