ecfb1911df080df3fe9dfcd52c40d413289b2310
Olamilekan <akintolaolamilekan51@gmail.com>
2023-08-17 22:23:14 +0100

    mysql

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,13 @@
+-- create database hbtn_0d_usa
+-- create a table states in hbtn_0d_usa
+
+
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+
+USE `hbtn_0d_usa`;
+
+CREATE TABLE
+    IF NOT EXISTS `states`
+    (id INT AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
+    name VARCHAR(256) NOT NULL
+    );
