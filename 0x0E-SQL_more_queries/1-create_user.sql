ca814d1e32d0f1d607c7fdf9dcc834f20815fe82
Olamilekan <akintolaolamilekan51@gmail.com>
2023-08-17 21:11:40 +0100

    mysql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,7 @@
 -- create a new user user_0d_1 if not exists
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+GRANT ALL PRIVILEGES
+    ON *.*
+    TO 'user_0d_1'@'localhost';
