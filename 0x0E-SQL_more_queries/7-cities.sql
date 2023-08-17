6f04bc1bc236c2c70b402c9ec4dcaea9db4073bf
Olamilekan <akintolaolamilekan51@gmail.com>
2023-08-17 22:39:14 +0100

    sql

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,15 @@
+-- create database hbtn_0d_usa
+-- create a table states in hbtn_0d_usa
+
+
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+
+USE `hbtn_0d_usa`;
+
+CREATE TABLE
+    IF NOT EXISTS `cities`
+    (id INT AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
+    state_id INT NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states.id
+    name VARCHAR(256) NOT NULL
+    );
