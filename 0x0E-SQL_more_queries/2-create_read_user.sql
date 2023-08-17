77106c7b94a4fc49025cd14e3eade3c91758789e
Olamilekan <akintolaolamilekan51@gmail.com>
2023-08-17 21:39:25 +0100

    mysql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,13 +3,13 @@
 -- grant them SELECT privilege to the database
 
 CREATE DATABASE
-    IF NOT EXISTS 'hbtn_0d_2';
+    IF NOT EXISTS hbtn_0d_2;
 
 CREATE USER
     IF NOT EXISTS 'user_0d_2'@'localhost'
     IDENTIFIED BY 'user_0d_2_pwd';
 
-GRANT SELECT ON 'hbtn_0d_2.*'
+GRANT SELECT ON hbtn_0d_2.*
     TO 'user_0d_2'@'localhost';
 
 FLUSH PRIVILEGES;
