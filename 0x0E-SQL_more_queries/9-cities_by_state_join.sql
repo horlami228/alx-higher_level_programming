8e9a7fe846fe4f8a1855fe3893b465fb852c4997
Olamilekan <akintolaolamilekan51@gmail.com>
2023-08-17 23:30:27 +0100

    sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,5 +3,5 @@
 SELECT cities.id, cities.name, states.name
     FROM cities
     INNER JOIN states
-    ON state.id = cities.state_id
+    ON states.id = cities.state_id
     ORDER BY cities.id ASC;
