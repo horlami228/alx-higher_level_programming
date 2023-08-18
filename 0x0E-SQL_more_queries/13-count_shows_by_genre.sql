c2c825c16f56c925764b06e0141f724730b489c3
Olamilekan <akintolaolamilekan51@gmail.com>
2023-08-18 23:22:53 +0100

    sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,9 +2,9 @@
 -- along with the count of shows with the genres
 -- order in descending orders by number of shows linked
 
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_rows
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
     FROM tv_genres
     INNER JOIN tv_show_genres
     ON tv_genres.id = tv_show_genres.genre_id
     GROUP BY tv_genres.name
-    ORDER BY number_of_rows DESC;
+    ORDER BY number_of_shows DESC;
