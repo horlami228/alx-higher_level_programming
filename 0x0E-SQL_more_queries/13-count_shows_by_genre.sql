a6bc6bc1a71367d592fc52aa9930f16c6a060c30
Olamilekan <akintolaolamilekan51@gmail.com>
2023-08-18 22:04:55 +0100

    sql

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- list all tv genres
+-- along with the count of shows with the genres
+
+
+SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS number_of_rows
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+GROUP BY tv_genres.name
+ORDER BY number_of_rows DESC;
