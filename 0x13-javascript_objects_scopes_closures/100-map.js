af614ad673cf92924a1a25d4390e9ecad6187a88
Olamilekan <akintolaolamilekan51@gmail.com>
2023-09-13 23:08:32 +0100

    js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
+
 const { list } = require('./100-data');
+console.log(list);
 const mapped = list.map((x) => { return x * list.indexOf(x); });
 console.log(mapped);
-console.log(list);
