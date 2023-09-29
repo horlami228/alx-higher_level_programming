c753e0df201dfb6be9541046b773c42d531da238
Olamilekan <akintolaolamilekan51@gmail.com>
2023-09-29 23:12:49 +0100

    check

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+    fetches from a url with the package requests
+"""
+import requests
+
+if __name__ == "__main__":
+    fetch = requests.get("https://alx-intranet.hbtn.io/status")
+    print(fetch.text)
+    print("Body response:")
+    print(f"    - type: {type(fetch.text)}")
+    print(f"    - content: {fetch.text}")
