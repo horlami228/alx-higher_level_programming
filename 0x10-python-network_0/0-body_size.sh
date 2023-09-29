15d31ff3629f7a046555f32ba568db34d47f9a6e
Olamilekan <akintolaolamilekan51@gmail.com>
2023-09-29 01:43:28 +0100

    add

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/bash
 # Takes in a URL
-# sends a request to it
-# displa the size of the response body in bytes
 
 curl -sI "$1" | grep "Content-Length" | cut -d " " -f2
