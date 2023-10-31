e23dd9ce540c8585875d7cbe029b345924b3b16b
Olamilekan <akintolaolamilekan51@gmail.com>
2023-10-31 12:13:57 +0100

    js

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = process.argv[2];
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error(err);
+  }
+
+  console.log('code:', response.statusCode);
+});
