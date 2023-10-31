ebaaad29bf0b2b5489800650e9492100f02ac0c9
Olamilekan <akintolaolamilekan51@gmail.com>
2023-11-01 00:10:15 +0100

    js

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const FilePath = process.argv[3];
+let content = '';
+
+request(url, (err, response, body) => {
+  if (err) {
+    console.error('error:', err);
+  } else {
+    // console.log(body)
+    content = body;
+    console.log(content);
+
+    fs.writeFile(FilePath, content, 'utf-8', (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+});
